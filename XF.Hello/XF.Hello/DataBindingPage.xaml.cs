d2b55dc709b0879e7fa19a4c2606c166c6a6d1e6
aledebarba <ale@alemacedo.com>
2017-10-18 22:19:58 -0200

    Nova tentativa de commit

---
+++ XF.Hello/XF.Hello/DataBindingPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Hello
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DataBindPage : ContentPage
+    {
+    
+        public DataBindPage()
+        {
+            Aluno aluno = InitAluno("Anderson Silva", "anderson@fiap.com");
+            BindingContext = aluno;
+
+            InitializeComponent();
+        }
+
+        private Aluno InitAluno(string nome, string email)
+        {
+            return new Aluno()
+            {
+                Id = Guid.NewGuid(),
+                Nome = nome,
+                Email = email
+            };
+        }
+    }
+}
